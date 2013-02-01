3e0408248839bd871acc4b33e5eda2a4f8b455d5
Dietmar Angerer <dangerer@gtn-solutions.com>
2013-02-01 11:03:10 +0100

    no settings (admin/blocks/config) necessary

--- version.php
+++ version.php
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version = 2013012900;
+$plugin->version = 2013012901;
 $exaport_release = "1.0";  // User-friendly version number
 $plugin->maturity  = MATURITY_BETA;
 $plugin->release  = "1.0";
