8ef175bcb3ce6ac9cc69525befc0d3b168dbac40
Florian Jungwirth <jungwirth.florian@gmail.com>
2013-07-16 16:43:29 +0200

    Update install.php
    download data xml during installation if it does not exist

--- db/install.php
+++ db/install.php
@@ -2,6 +2,14 @@
 function xmldb_block_exacomp_install() {
 	global $CFG;
 	require_once $CFG->dirroot . '/blocks/exacomp/lib/xmllib.php';
-	
+
+	if(!file_exists($CFG->dirroot . '/blocks/exacomp/xml/exacomp_data.xml')) {
+		$xmlserverurl = "https://raw.github.com/gtn/edustandards/master/austria/exacomp_data.xml";
+		if($xmlserverurl) {
+			$xml = file_get_contents($xmlserverurl);
+			if($xml)
+				file_put_contents($CFG->dirroot.'/blocks/exacomp/xml/exacomp_data.xml',$xml);
+		}
+	}
 	block_exacomp_xml_do_import(null,1,1);
 }
