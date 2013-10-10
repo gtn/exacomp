ecd7f67438b424628a046fb4665430697cfeef1a
Dietmar Angerer <uab.katzinger@gmail.com>
2013-10-10 10:30:21 +0200

    change same langstrings

--- lang/de/block_exacomp_overlaystatic.php
+++ lang/de/block_exacomp_overlaystatic.php
@@ -1,10 +1,2 @@
  <?php
- 
-$string['admintabschooltype'] = 'Gegenstand-Auswahl';
-$string['teachertabassigncompetenceexamples'] = 'Lernmaterialien';
-$string['teachertabselection'] = 'Kompetenzbereich';
-$string['examples'] = 'Lernmaterialien';
-$string['subject'] = 'Kompetenzbereiche';
-$string['anhang'] = 'Zusatzmaterial';
-
 ?>
