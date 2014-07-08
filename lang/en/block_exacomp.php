227b7a267460d528d945c5f14d2823193b3f8670
Florian Jungwirth <jungwirth.florian@gmail.com>
2014-07-08 13:09:32 +0200

    page template

---
+++ lang/en/block_exacomp.php
@@ -0,0 +1,52 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_exacomp', language 'en'
+ *
+ * @package   block_exacomp
+ * @copyright Florian Jungwirth <danielneis@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['newblock:addinstance'] = 'Add a exabis competencies block';
+$string['newblock:myaddinstance'] = 'Add a exabis competencies block to my moodle';
+$string['pluginname'] = 'exabis competencies';
+
+// TABS and PAGE IDENTIFIERS
+// Admin Tabs
+$string['tab_admin_import'] = 'Import';
+$string['tab_admin_configuration'] = 'Configuration';
+
+//Teacher Tabs
+$string['tab_teacher_settings'] = 'Settings';
+$string['tab_teacher_settings_configuration'] = 'configuration';
+$string['tab_teacher_settings_selection'] = 'Subject selection';
+$string['tab_teacher_settings_assignactivities'] = 'Assign activities';
+$string['tab_teacher_settings_badges'] = 'Edit badges';
+
+//Student Tabs
+$string['tab_student_all'] = 'All gained competencies';
+
+//Generic Tabs (used by Teacher and Students)
+$string['tab_competence_grid'] = 'Competence grid';
+$string['tab_competence_overview'] = 'Overview of competencies';
+$string['tab_competence_details'] = 'Detailed competence-view';
+$string['tab_examples'] = 'Examples and tasks';
+$string['tab_learning_agenda'] = 'Learning agenda';
+$string['tab_badges'] = 'My badges';
+$string['tab_competence_profile'] = 'Competence profile';
---
+++ lib/lib.php
@@ -0,0 +1,33 @@
+<?php
+
+function block_exacomp_build_navigation_tabs($context,$courseid) {
+	
+	if(has_capability('block/exacomp:admin', $context)) {
+		$rows = array(
+				new tabobject('tab_admin_import', new moodle_url('/blocks/exacomp/edit_config.php',array("courseid"=>$courseid)),get_string('tab_admin_import','block_exacomp')),
+				new tabobject('tab_admin_configuration', new moodle_url('/blocks/exacomp/import.php',array("courseid"=>$courseid)),get_string('tab_admin_configuration','block_exacomp'))
+		);
+	}
+	if (has_capability('block/exacomp:teacher', $context)) {
+		$rows = array(
+				new tabobject('tab_competence_overview', new moodle_url('/blocks/exacomp/assign_competencies.php',array("courseid"=>$courseid)),get_string('tab_competence_overview','block_exacomp')),
+				new tabobject('tab_competence_details', new moodle_url('/blocks/exacomp/edit_students.php',array("courseid"=>$courseid)),get_string('tab_competence_details','block_exacomp')),
+				new tabobject('tab_competence_grid', new moodle_url('/blocks/exacomp/competence_grid.php',array("courseid"=>$courseid)),get_string('tab_competence_grid','block_exacomp')),
+				new tabobject('tab_examples', new moodle_url('/blocks/exacomp/view_examples.php',array("courseid"=>$courseid)),get_string('tab_examples','block_exacomp')),
+				new tabobject('tab_learning_agenda', new moodle_url('/blocks/exacomp/learningagenda.php',array("courseid"=>$courseid)),get_string('tab_learning_agenda','block_exacomp')),
+				new tabobject('tab_badges', new moodle_url('/blocks/exacomp/my_badges.php',array("courseid"=>$courseid)),get_string('tab_badges','block_exacomp')),
+				new tabobject('tab_teacher_settings', new moodle_url('/blocks/exacomp/edit_course.php',array("courseid"=>$courseid)),get_string('tab_teacher_settings','block_exacomp'))
+		);
+	} elseif (has_capability('block/exacomp:student', $context)) {
+		$rows = array(
+				new tabobject('tab_competence_overview', new moodle_url('/blocks/exacomp/assign_competencies.php',array("courseid"=>$courseid)),get_string('tab_competence_overview','block_exacomp')),
+				new tabobject('tab_competence_details', new moodle_url('/blocks/exacomp/edit_students.php',array("courseid"=>$courseid)),get_string('tab_competence_details','block_exacomp')),
+				new tabobject('tab_student_all', new moodle_url('/blocks/exacomp/all_gained_competencies_course_based.php',array("courseid"=>$courseid)),get_string('tab_student_all','block_exacomp')),
+				new tabobject('tab_competence_grid', new moodle_url('/blocks/exacomp/competence_grid.php',array("courseid"=>$courseid)),get_string('tab_competence_grid','block_exacomp')),
+				new tabobject('tab_learning_agenda', new moodle_url('/blocks/exacomp/learningagenda.php',array("courseid"=>$courseid)),get_string('tab_learning_agenda','block_exacomp')),
+				new tabobject('tab_badges', new moodle_url('/blocks/exacomp/my_badges.php',array("courseid"=>$courseid)),get_string('tab_badges','block_exacomp'))
+		);
+	}
+	
+	return $rows;
+}
