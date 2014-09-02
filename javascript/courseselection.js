cabc2686b37cc16b831cdda6fff2f96e9b04085d
Florian Jungwirth <jungwirth.florian@gmail.com>
2014-09-02 13:00:40 +0200

    reopen row groups for comp overview and course selection

--- javascript/courseselection.js
+++ javascript/courseselection.js
@@ -40,6 +40,22 @@
 	$(function(){
 		var $form = $('#course-selection');
 
+		// reopen selected groups
+		$form.find('.rowgroup-content').has(':checkbox:checked').each(function(){
+			$.each(this.className.match(/rowgroup-content-([0-9]+)/g), function(tmp, match){
+				match.match(/([0-9]+)/);
+				var id = RegExp.$1;
+				$form.find('.rowgroup-header-'+id).addClass('open');
+				$form.find('.rowgroup-content-'+id).show();
+			});
+		});
+		// opening: hide all subs which are still closed
+		$('.rowgroup-header').not('.open').each(function(){
+			var id = this.className.replace(/^.*rowgroup-header-([0-9]+).*$/, '$1');
+			$('.rowgroup-content-'+id).hide();
+		});
+		
+		
 		// submit open groups
 		$form.submit(function(){
 			
