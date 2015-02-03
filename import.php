7d01c08f382dedf50c729f37c8c2131001bfb392
Florian Jungwirth <jungwirth.florian@gmail.com>
2015-02-03 13:49:48 +0100

    permission for school specific import

--- import.php
+++ import.php
@@ -129,9 +129,12 @@ if($isAdmin || block_exacomp_check_customupload()) {
 							$url = 'edit_course.php';
 						}
 						
-						echo $OUTPUT->box(get_string("importsuccess", "block_exacomp").html_writer::empty_tag('br')
-							.html_writer::empty_tag('img', array('src'=>new moodle_url('/blocks/exacomp/pix/one_admin.png'), 'alt'=>'', 'width'=>'60px', 'height'=>'60px'))
-							.html_writer::link(new moodle_url($url, array('courseid'=>$courseid, 'fromimport'=>1)), $string));
+						$html = get_string("importsuccess", "block_exacomp").html_writer::empty_tag('br');
+						if($isAdmin)
+							$html .= html_writer::empty_tag('img', array('src'=>new moodle_url('/blocks/exacomp/pix/one_admin.png'), 'alt'=>'', 'width'=>'60px', 'height'=>'60px'))
+							.html_writer::link(new moodle_url($url, array('courseid'=>$courseid, 'fromimport'=>1)), $string);
+						
+						echo $OUTPUT->box($html);
 					}
 					else {
 						echo $OUTPUT->box(get_string("importfail", "block_exacomp"));
