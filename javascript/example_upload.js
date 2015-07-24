b43dfa254253e581ad002ee440a9a61178a0dede
Michaela Murauer <mmurauer@gtn-solutions.com>
2015-07-24 05:37:56 +0200

    open list on load, when checkbox checked

--- javascript/example_upload.js
+++ javascript/example_upload.js
@@ -4,5 +4,17 @@
 (function($){
 	$( window ).load(function() {
 		CollapsibleLists.apply();
+		
+		// reopen selected li
+		$( "input[type=checkbox]" ).each(function( ) {
+			if($(this).attr('checked')){
+				$(this).parents('ul').addClass('collapsibleListOpen');
+				$(this).parents('ul').removeClass('collapsibleListClosed');
+				$(this).parents('ul').attr('style', 'display:block');
+				
+			}
+		});
+		
 	});
+	
 })(jQueryExacomp);
