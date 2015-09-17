3b774ef973280f31593efaaae37de29e8d5ebaa0
Florian Jungwirth <jungwirth.florian@gmail.com>
2015-09-17 15:59:38 +0200

    lang update

--- lang/de/block_exacomp.php
+++ lang/de/block_exacomp.php
@@ -269,7 +269,7 @@ $string['newsubmission'] = 'Erneute Abgabe';
 $string['example_submission_header']  = 'Aufgabe {$a} bearbeiten';
 $string['example_submission_info'] = 'Du bist dabei die Aufgabe {$a} zu bearbeiten. Deine Abgabe landet im ePortfolio und kann dort von dir und deinem Lehrern eingesehen werden.';
 $string['example_submission_subject'] = 'Neue Abgabe';
-$string['example_submission_message'] = 'Im Kurs {$a->course} wurde vom Schüler {$a->student} eine neue Abgabe eingereicht.';
+$string['example_submission_message'] = 'Im Kurs {$a->course} wurde vom Schüler/innen {$a->student} eine neue Abgabe eingereicht.';
 
 $string['submissionmissing']  = 'Es müssen zumindest ein Link oder eine Datei abgegeben werden';
 
@@ -471,7 +471,7 @@ $string['weekly_schedule_link_to_grid'] = 'Um den Wochenplan zu befüllen in den
 $string['pre_planning_storage'] = 'Vorplanungsspeicher';
 $string['pre_planning_storage_added'] = 'Lernmaterial wurde zum Vorplanungsspeicher hinzugefügt.';
 $string['pre_planning_storage_already_contains'] = 'Lernmateriel bereits im Vorplanungsspeicher enthalten.';
-$string['save_pre_planning_selection'] = 'Ausgewählte Beispiele auf den Wochenplan der ausgewählten Schüler legen';
+$string['save_pre_planning_selection'] = 'Ausgewählte Beispiele auf den Wochenplan der ausgewählten Schüler/innen legen';
 $string['empty_pre_planning_storage'] = 'Vorplanungsspeicher leeren';
 $string['noschedules_pre_planning_storage'] = 'Vorplanungsspeicher wurde geleert, legen Sie über die Kompetenzraster neue Lernmaterialien in den Vorplanungsspeicher.';
 $string['empty_trash'] = 'Papierkorb leeren';
