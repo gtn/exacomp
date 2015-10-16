f439cecf3dd6e1ae1e1d821720f8997c6d7ea768
dprieler <dprieler@gmail.com>
2015-10-16 16:29:07 +0200

    test scripts

---
+++ backup/test_backup.php
@@ -0,0 +1,17 @@
+<?php
+ 
+require_once('../inc.php');
+require_once($CFG->dirroot . '/backup/util/includes/backup_includes.php');
+ 
+if (!is_siteadmin()) {
+	die('Pls login first!');
+}
+
+$course_id = 2; // Set this to one existing choice cmid in your dev site
+$user_doing_the_backup   = 2; // Set this to the id of your admin accouun
+ 
+$bc = new backup_controller(backup::TYPE_1COURSE, $course_id, backup::FORMAT_MOODLE,
+                            backup::INTERACTIVE_NO, backup::MODE_GENERAL, $user_doing_the_backup);
+$bc->execute_plan();
+
+die('done');
---
+++ backup/test_restore.php
@@ -0,0 +1,29 @@
+<?php
+ 
+require_once('../inc.php');
+require_once($CFG->dirroot . '/backup/util/includes/restore_includes.php');
+ 
+if (!is_siteadmin()) {
+	die('Pls login first!');
+}
+
+// Transaction.
+$transaction = $DB->start_delegated_transaction();
+ 
+// Create new course.
+$folder             = '61ccef7ce9f223715890ee752aa30db3'; // as found in: $CFG->dataroot . '/temp/backup/' 
+$categoryid         = 1; // e.g. 1 == Miscellaneous
+$userdoingrestore   = 2; // e.g. 2 == admin
+$courseid           = 52; // restore_dbops::create_new_course('', '', $categoryid);
+echo $courseid.' ';
+// Restore backup into course.
+$controller = new restore_controller($folder, $courseid, 
+        backup::INTERACTIVE_NO, backup::MODE_SAMESITE, $userdoingrestore,
+        backup::TARGET_NEW_COURSE);
+$controller->execute_precheck();
+$controller->execute_plan();
+ 
+// Commit.
+$transaction->allow_commit();
+
+die('done');
