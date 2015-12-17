2a7571a17e3b614702f97eba575d1837cc2fac59
Dietmar Angerer <uab.katzinger@gmail.com>
2015-12-17 21:04:33 +0100

    increase version number

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 
 // This file is part of Moodle - http://moodle.org/
 //
@@ -18,15 +18,15 @@
 /**
  * Version details
  *
- * @package    block_exacomp
+ * @package	block_exacomp
  * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license	http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015051700;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2013111800;        // Requires this Moodle version 2.6
+$plugin->version   = 2015121703;		// The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2015051100;		// Requires this Moodle version 2.6
 $plugin->component = 'block_exacomp'; 	// Full name of the plugin (used for diagnostics)
 $plugin->cron = 259200; 				//259200sec = 3 days
 
