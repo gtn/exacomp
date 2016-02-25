30e4c3c52f6f238c9fab7e6ac00a4e33d0d46035
dprieler <dprieler@gmail.com>
2016-02-25 14:56:59 +0100

    set new copyright

--- lang/copyright.php
+++ lang/copyright.php
@@ -1,13 +1,29 @@
 <?php
-/*
- * copyright exabis
- */
+// This file is part of Exabis Competencies
+//
+// (c) 2016 exabis internet solutions <info@exabis.at>
+//
+// Exabis Comeptencies is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This script is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You can find the GNU General Public License at <http://www.gnu.org/licenses/>.
+//
+// This copyright notice MUST APPEAR in all copies of the script!
 
 require __DIR__.'/../inc.php';
 
 $searchPath = dirname(__DIR__);
 $Directory = new RecursiveDirectoryIterator($searchPath);
 $Iterator = new RecursiveIteratorIterator($Directory);
+
+$overwriteAll = true;
 
 foreach ($Iterator as $file) {
 	$file = $file->getPathname();
@@ -22,13 +38,22 @@ foreach ($Iterator as $file) {
 	$content = preg_replace('!^<\?php\s+!', '', $content);
 	$content = preg_replace('!\?>\s*$!', '', $content);
 
+	$copyrightFound = false;
+
 	while (true) {
 		$content = trim($content);
 
 		if (true) {
-			if (preg_match('!^/\*.*exabis.*\*/!sU', $content, $matches)) {
+			if (preg_match('!^/\*.*exabis.*\*/!isU', $content, $matches)) {
+				$copyrightFound = true;
 				echo $matches[0]."\n";
 				$content = preg_replace('!^/\*.*\*/!sU', '', $content);
+				continue;
+			}
+			if (preg_match('!^(//.*\n)*//.*exabis.*\n(//.*\n)*!i', $content, $matches)) {
+				$copyrightFound = true;
+				echo $matches[0]."\n";
+				$content = preg_replace('!^(//.*\n)*//.*exabis.*\n(//.*\n)*!i', '', $content);
 				continue;
 			}
 		} else {
@@ -46,11 +71,29 @@ foreach ($Iterator as $file) {
 		break;
 	}
 
+	if (!$copyrightFound) continue;
+
 	// copyright
 	$copyright =
-"/*
- * copyright exabis
- */";
+trim("
+// This file is part of Exabis Competencies
+//
+// (c) 2016 exabis internet solutions <info@exabis.at>
+//
+// Exabis Comeptencies is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This script is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You can find the GNU General Public License at <http://www.gnu.org/licenses/>.
+//
+// This copyright notice MUST APPEAR in all copies of the script!
+");
 
 	$content = "$copyright\n\n".$content."\n";
 
@@ -59,8 +102,11 @@ foreach ($Iterator as $file) {
 	}
 
 	file_put_contents($file, $content);
-	// echo $content;
-	// exit;
+	/*
+	echo "-----------------------------------\n";
+	echo $content;
+	exit;
 	// $i++;
 	// if ($i> 4) exit;
+	/* */
 }
--- message_to_course.php
+++ message_to_course.php
@@ -1,7 +1,21 @@
 <?php
-/*
- * copyright exabis
- */
+// This file is part of Exabis Competencies
+//
+// (c) 2016 exabis internet solutions <info@exabis.at>
+//
+// Exabis Comeptencies is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This script is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You can find the GNU General Public License at <http://www.gnu.org/licenses/>.
+//
+// This copyright notice MUST APPEAR in all copies of the script!
 
 require __DIR__.'/inc.php';
 require_once __DIR__.'/example_upload_form.php';
