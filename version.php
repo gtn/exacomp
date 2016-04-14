4cdb973a13ea0b424fb6a82d5990b231c21cda18
dprieler <dprieler@gmail.com>
2016-04-14 11:51:52 +0200

    bump version

--- version.php
+++ version.php
@@ -19,10 +19,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016040600;		// The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2015051100;		// Requires this Moodle version 2.6
-$plugin->component = 'block_exacomp'; 	// Full name of the plugin (used for diagnostics)
-$plugin->cron = 259200; 				//259200sec = 3 days
-
+$plugin->component = 'block_exacomp';
+$plugin->release = '3.0.1';
+$plugin->version   = 2016041400;
+$plugin->requires  = 2015051100;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v2.6.5-r1'; 			// This is our first release for Moodle 2.7.x branch.
+$plugin->cron = 259200;
