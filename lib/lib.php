8cd8b4210db085255af7c85bdd83bbffb03c2127
Florian Jungwirth <jungwirth.florian@gmail.com>
2016-07-14 10:43:30 +0200

    bug fix
    Do not display niveau evaluation icons if no niveaus are used

--- lib/lib.php
+++ lib/lib.php
@@ -6344,6 +6344,9 @@ function block_exacomp_get_courseids_by_descriptor($descriptorid){
 function block_exacomp_get_html_for_niveau_eval($evaluation){
 	$evaluation_niveau_type = block_exacomp_evaluation_niveau_type();
 	
+	if($evaluation_niveau_type == 0)
+		return;
+
 	//predefined pictures 
 		$grey_1_src = '/blocks/exacomp/pix/compprof_rating_teacher_grey_1_'.$evaluation_niveau_type.'.png';
 		$grey_2_src = '/blocks/exacomp/pix/compprof_rating_teacher_grey_2_'.$evaluation_niveau_type.'.png';
