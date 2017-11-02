d17e89ef8f0245fc770812bd4c8fe8698ed43c41
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2017-11-02 12:53:09 +0100

    Added classes to GroupReport output

--- lib/lib.php
+++ lib/lib.php
@@ -7736,7 +7736,19 @@ function block_exacomp_group_reports_result($filter) {
 					return;
 				}
 
-				echo '<tr>';
+				
+				//item_type is needed to distinguish between topics, parent descripors and child descriptors --> important for css-styling
+				$item_type = $item::TYPE;
+				if ($item_type == BLOCK_EXACOMP_TYPE_SUBJECT) {
+				    echo '<tr class="exarep_subject_row">';
+				}else if ($item_type == BLOCK_EXACOMP_TYPE_TOPIC) {
+				    echo '<tr class="exarep_topic_row">';
+				}else if($item_type == BLOCK_EXACOMP_TYPE_DESCRIPTOR && $level <= 2) {
+				    echo '<tr class="exarep_descriptor_parent_row">';
+				}else if($item_type == BLOCK_EXACOMP_TYPE_DESCRIPTOR && $level > 2) {
+				    echo '<tr class="exarep_descriptor_child_row">';
+				}
+				
 				echo '<td class="exarep_descriptor" style="white-space: nowrap">'.$item->get_numbering();
 				echo '<td class="exarep_descriptorText" style="padding-left: '.($level * 20).'px">'.$item->title;
 				if (@$filter['time']['active']) {
