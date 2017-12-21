f0da11d68b666947d137121692d73168c8b3a862
Dietmar Angerer <uab.katzinger@gmail.com>
2017-12-21 21:31:11 +0100

    init2

--- version.php
+++ version.php
@@ -12,7 +12,6 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-//
 // You can find the GNU General Public License at <http://www.gnu.org/licenses/>.
 
 // This copyright notice MUST APPEAR in all copies of the script!
