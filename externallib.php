bda6e594c32642aecdba6c55460eae2208c880a7
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2018-07-20 14:57:22 +0200

    comment in correct place

--- externallib.php
+++ externallib.php
@@ -3234,19 +3234,18 @@ class block_exacomp_external extends external_api {
 
 		$example = $DB->get_record(BLOCK_EXACOMP_DB_EXAMPLES, array('id' => $exampleid));
 
-		if ($forall) {
-			//Add to the teacher planning Storage, not to the students themselves    otherwise they would have it in the schedule as well as in the pool which 
-			//could lead to mnay issues (not technical, but logical for them)
- 		    //block_exacomp_add_example_to_schedule($userid, $exampleid, $creatorid, $courseid, null, null, 0);
-			
+		if ($forall) {	
  			$students = block_exacomp_get_students_by_course($courseid);
-
+            //Add to all the students
 			foreach ($students as $student) {
 			    if (block_exacomp_is_example_visible($courseid, $exampleid, $student->id)) {
 			        block_exacomp_add_example_to_schedule($student->id, $exampleid, $creatorid, $courseid, null, null, 0);
 				}
 			}
 		} else {
+		    //Add to the teacher planning Storage, not to the students themselves    otherwise they would have it in the schedule as well as in the pool which 
+		    //could lead to mnay issues (not technical, but logical for them)
+		    //block_exacomp_add_example_to_schedule($userid, $exampleid, $creatorid, $courseid, null, null, 0);
 		    if (block_exacomp_is_example_visible($courseid, $exampleid, $userid)) {
 		        block_exacomp_add_example_to_schedule($userid, $exampleid, $creatorid, $courseid, null, null, 0);
 			}
