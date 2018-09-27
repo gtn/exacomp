632f271964ab34695432fb88639e18459acf92f4
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2018-09-27 11:28:30 +0200

    cleanup

--- lib/lib.php
+++ lib/lib.php
@@ -8974,38 +8974,7 @@ function block_exacomp_get_date_of_birth($userid) {
 			JOIN {".BLOCK_EXACOMP_DB_DESCEXAMP."} mm ON e.id=mm.exampid AND mm.id_foreign=?"
          , array($crosssubjectid));
      
-     
-//      $examples = \block_exacomp\example::get_objects_sql(
-//          "SELECT DISTINCT de.id as deid, e.id, e.title, e.externalurl, e.source, e.sourceid,
-// 			e.externalsolution, e.externaltask, e.completefile, e.description, e.creatorid, e.iseditable, e.tips, e.timeframe, e.author
-// 			, de.sorting
-// 			FROM {".BLOCK_EXACOMP_DB_EXAMPLES."} e
-// 			JOIN {".BLOCK_EXACOMP_DB_DESCEXAMP."} de ON e.id=de.exampid AND de.descrid=?"
-//          ." WHERE "
-//          ." e.source != ".BLOCK_EXACOMP_EXAMPLE_SOURCE_USER." AND "
-//          .($showallexamples ? " 1=1 " : " e.creatorid > 0")
-//          ." ORDER BY de.sorting"
-//          , array($descriptor->id, $courseid, $courseid));
-     
-     
-//      $sql = "select s.*,
-// 				e.title, e.id as exampleid, e.source AS example_source, evis.visible,
-// 				eval.student_evaluation, eval.teacher_evaluation, eval.evalniveauid, evis.courseid, s.id as scheduleid,
-// 				e.externalurl, e.externaltask, e.description
-// 			FROM {block_exacompschedule} s
-// 			JOIN {block_exacompexamples} e ON e.id = s.exampleid
-// 			JOIN {".BLOCK_EXACOMP_DB_EXAMPVISIBILITY."} evis ON evis.exampleid= e.id AND evis.studentid=0 AND evis.visible = 1 AND evis.courseid=?
-// 			LEFT JOIN {block_exacompexameval} eval ON eval.exampleid = s.exampleid AND eval.studentid = s.studentid AND eval.courseid = s.courseid
-// 			WHERE s.studentid = ? AND s.deleted = 0 AND s.is_pps = 0 AND (
-// 				-- noch nicht auf einen tag geleg
-// 				(s.start IS null OR s.start=0)
-// 				-- oder auf einen tag der vorwoche gelegt und noch nicht evaluiert
-// 				OR (s.start < ? AND (eval.teacher_evaluation IS NULL OR eval.teacher_evaluation=0))
-// 			)
-// 			ORDER BY s.id";
-     
      return $examples;
-     //return $DB->get_records_sql($sql, array($courseid, $studentid, $beginning_of_week));
  }
  
 //  /**
