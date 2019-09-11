b11d08a8e48b033916785ecc6470994b9149ceb7
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2019-09-11 11:02:12 +0200

    version

--- version.php
+++ version.php
@@ -20,6 +20,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exacomp';
 $plugin->release = '4.6.3';
-$plugin->version   = 2019090302;
+$plugin->version   = 2109091100;
 $plugin->requires  = 2015051100;
 $plugin->maturity = MATURITY_STABLE;
