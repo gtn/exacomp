671e3d47722edca0ee44ca0e592d65ba9821777d
Dietmar Angerer <uab.katzinger@gmail.com>
2019-10-24 17:14:58 +0200

    some wordings

--- lang/total.php
+++ lang/total.php
@@ -495,7 +495,7 @@ return [
         'assessment scheme points, limit for input',
     ],
     'settings_assessment_points_negativ' => [
-        'Negative Beaurteilung Punkte',
+        'Negative Beurteilung Punkte',
         'Fail value for Points',
     ],
     'settings_assessment_points_negativ_description' => [
@@ -511,7 +511,7 @@ return [
         'assessment scheme grade, limit for input',
     ],
     'settings_assessment_grade_negativ' => [
-        'Negative Beaurteilung Noten',
+        'Negative Beurteilung Noten',
         'Fail value for grade',
     ],
     'settings_assessment_grade_negativ_description' => [
@@ -567,7 +567,7 @@ return [
             'very good, good, satisfactory, sufficient, deficient, insufficient',
     ],
     'settings_assessment_grade_verbose_negative' => [
-            'Negative Beaurteilung Verbalisierung',
+            'Negative Beurteilung Verbalisierung',
             'Fail verbalized grade (EN)',
     ],
     'settings_assessment_grade_verbose_negative_description' => [
