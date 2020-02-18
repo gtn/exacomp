a22256ebfe0c9b1539c210d842950cac088e89a6
GTN-FabioPernegger <fpernegger@gtn-solutions.com>
2020-02-18 15:27:30 +0100

    fix for backup

--- backup/moodle2/restore_exacomp_stepslib.php
+++ backup/moodle2/restore_exacomp_stepslib.php
@@ -17,8 +17,9 @@
 //
 // This copyright notice MUST APPEAR in all copies of the script!
 
-require_once __DIR__."/../../lib/lib.php";
-require_once __DIR__."/../../classes/data.php";
+require_once __DIR__."/../../inc.php";
+
+use block_exacomp\globals as g;
 
 class restore_exacomp_block_structure_step extends restore_structure_step {
     
@@ -32,14 +33,16 @@ class restore_exacomp_block_structure_step extends restore_structure_step {
         $paths[] = new restore_path_element('topic', '/block/exacomp/topics/topic');
         $paths[] = new restore_path_element('compactiv_mm', '/block/exacomp/activities/compactiv_mm');
         $paths[] = new restore_path_element('taxonomy', '/block/exacomp/taxonomies/taxonomy');
+        $paths[] = new restore_path_element('evaluation', '/block/exacomp/evaluations/evaluation');
         
         // Return the paths wrapped into standard activity structure
         return $paths;
     }
     
-    protected function get_db_record($table, $data, $prefix = "") {
+    protected function get_db_record_from_sourceid($table, $data, $prefix = "") {
         global $DB;
         
+        \block_exacomp\data::prepare();
         if (!$where = block_exacomp\data_course_backup::parse_sourceid($data, $prefix)) {
             return null;
         }
@@ -54,10 +57,10 @@ class restore_exacomp_block_structure_step extends restore_structure_step {
         
         $taxonomies = array();
         if (isset($data->exacomp['taxonomies']['taxonomy'])) {
-            foreach($data->exacomp['taxonomies']['taxonomy'] as $taxonomy) {
+            foreach ($data->exacomp['taxonomies']['taxonomy'] as $taxonomy) {
                 $taxonomy = (object)$taxonomy;
                 
-                if (!$dbTaxonomy = $this->get_db_record(\block_exacomp\DB_TAXONOMIES, $taxonomy)) {
+                if (!$dbTaxonomy = $this->get_db_record_from_sourceid(BLOCK_EXACOMP_DB_TAXONOMIES, $taxonomy)) {
                     continue;
                 }
                 
@@ -71,16 +74,15 @@ class restore_exacomp_block_structure_step extends restore_structure_step {
             $settings = (object)reset($settings);
             unset($settings->courseid);
             unset($settings->id);
+            $settings->filteredtaxonomies = json_encode($taxonomies ? $taxonomies : [BLOCK_EXACOMP_SHOW_ALL_TAXONOMIES]);
             
-            $settings->filteredtaxonomies = $taxonomies ? json_encode($taxonomies) : array(SHOW_ALL_TAXONOMIES);
-            
-            block_exacomp\globals::$DB->insert_or_update_record(\block_exacomp\DB_SETTINGS, $settings, array('courseid'=>$this->get_courseid()));
+            g::$DB->insert_or_update_record(BLOCK_EXACOMP_DB_SETTINGS, $settings, array('courseid' => $this->get_courseid()));
         }
         
         if (isset($data->exacomp['mdltypes']['mdltype'])) {
             foreach ($data->exacomp['mdltypes']['mdltype'] as $mdltype) {
                 $mdltype = (object)$mdltype;
-                if (!$schooltype = $this->get_db_record(\block_exacomp\DB_SCHOOLTYPES, $mdltype)) {
+                if (!$schooltype = $this->get_db_record_from_sourceid(BLOCK_EXACOMP_DB_SCHOOLTYPES, $mdltype)) {
                     continue;
                 }
                 
@@ -91,23 +93,26 @@ class restore_exacomp_block_structure_step extends restore_structure_step {
             }
         }
         
+        $topicids = [];
         if (isset($data->exacomp['topics']['topic'])) {
             
-            foreach($data->exacomp['topics']['topic'] as $topic) {
+            foreach ($data->exacomp['topics']['topic'] as $topic) {
                 $topic = (object)$topic;
                 
-                if (!$dbTopic = $this->get_db_record(\block_exacomp\DB_TOPICS, $topic)) {
+                if (!$dbTopic = $this->get_db_record_from_sourceid(BLOCK_EXACOMP_DB_TOPICS, $topic)) {
                     continue;
                 }
                 
-                block_exacomp\globals::$DB->insert_or_update_record(\block_exacomp\DB_COURSETOPICS, array('topicid' => $dbTopic->id, 'courseid' => $this->get_courseid()));
+                $topicids[] = $dbTopic->id;
             }
         }
+        
+        block_exacomp_set_coursetopics($this->get_courseid(), $topicids, true);
         
         // hack
         $GLOBALS['block_exacomp_imported_activities'] = array();
         if (isset($data->exacomp['activities']['compactiv_mm'])) {
-            $course = $DB->get_record("course",array("id"=>$this->get_courseid()));
+            $course = $DB->get_record("course", array("id" => $this->get_courseid()));
             
             $DB->execute("
 					DELETE FROM {block_exacompcompactiv_mm} WHERE activityid IN
@@ -117,15 +122,15 @@ class restore_exacomp_block_structure_step extends restore_structure_step {
             foreach ($data->exacomp['activities']['compactiv_mm'] as $descractiv_mm) {
                 $descractiv_mm = (object)$descractiv_mm;
                 
-                if ($descractiv_mm->comptype == \block_exacomp\TYPE_DESCRIPTOR) {
-                    $table = \block_exacomp\DB_DESCRIPTORS;
-                } else if ($descractiv_mm->comptype == \block_exacomp\TYPE_TOPIC) {
-                    $table = \block_exacomp\DB_TOPICS;
+                if ($descractiv_mm->comptype == BLOCK_EXACOMP_TYPE_DESCRIPTOR) {
+                    $table = BLOCK_EXACOMP_DB_DESCRIPTORS;
+                } elseif ($descractiv_mm->comptype == BLOCK_EXACOMP_TYPE_TOPIC) {
+                    $table = BLOCK_EXACOMP_DB_TOPICS;
                 } else {
                     print_error("unknown comptype {$descractiv_mm->comptype}");
                 }
                 
-                if (!$source_desc = $this->get_db_record($table, $descractiv_mm, 'comp')) {
+                if (!$source_desc = $this->get_db_record_from_sourceid($table, $descractiv_mm, 'comp')) {
                     continue;
                 }
                 
@@ -138,6 +143,26 @@ class restore_exacomp_block_structure_step extends restore_structure_step {
                 $descractiv_mm->id = $DB->insert_record('block_exacompcompactiv_mm', $descractiv_mm);
                 
                 $GLOBALS['block_exacomp_imported_activities'][$descractiv_mm->id] = $descractiv_mm;
+            }
+        }
+        
+        if (isset($data->exacomp['evaluations']['evaluation'])) {
+            foreach ($data->exacomp['evaluations']['evaluation'] as $evaluation) {
+                $evaluation = (object)$evaluation;
+                
+                if (!$table = block_exacomp_get_db_table_from_type($evaluation->comptype)) {
+                    continue;
+                }
+                
+                if (!$competence = $this->get_db_record_from_sourceid($table, $evaluation)) {
+                    // competence not found
+                    // also checks for local competence etc.
+                    continue;
+                }
+                
+                block_exacomp_set_comp_eval($this->get_courseid(), $evaluation->role,
+                    $this->get_mappingid('user', $evaluation->userid) ?: $evaluation->userid,
+                    $evaluation->comptype, $competence->id, $evaluation);
             }
         }
     }
