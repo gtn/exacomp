8fc08251799e5485ab988ae945e3420bea3c2dbf
Dietmar Angerer <uab.katzinger@gmail.com>
2020-12-17 13:32:35 +0100

    increase version number

--- version.php
+++ version.php
@@ -20,6 +20,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exacomp';
 $plugin->release = '4.6.5';
-$plugin->version   = 2020120400;
+$plugin->version   = 2020121700;
 $plugin->requires  = 2015051100;
 $plugin->maturity = MATURITY_STABLE;
