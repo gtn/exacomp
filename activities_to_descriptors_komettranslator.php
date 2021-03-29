34b5e67860dcda93b632221b222f5b226c12dbb8
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2021-03-29 13:57:30 +0200

    activities_to_compettranslator started

---
+++ activities_to_descriptors_komettranslator.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Exabis Competence Grid
+//
+// (c) 2016 GTN - Global Training Network GmbH <office@gtn-solutions.com>
+//
+// Exabis Competence Grid is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This script is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You can find the GNU General Public License at <http://www.gnu.org/licenses/>.
+//
+// This copyright notice MUST APPEAR in all copies of the script!
+
+require __DIR__.'/inc.php';
+
+//TODO: find all activities which have competencies
+//for these competencies: find out if thy are in local_komettranslator
+//if they are, find the related descriptorid
+//for each activityid call the function with the related descriptorids
+//done
+//next -> gradings
+block_exacomp_relate_example_to_activity(5,8,array(625));
+
