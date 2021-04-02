58c291793582d9a94b4bec73e3859a35608a5e76
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2021-04-02 12:56:03 +0200

    activities_to_descriptors_komettranslator: creating examples in descriptors implemented

--- activities_to_descriptors_komettranslator.php
+++ activities_to_descriptors_komettranslator.php
@@ -17,7 +17,7 @@
 //
 // This copyright notice MUST APPEAR in all copies of the script!
 
-require __DIR__.'/inc.php';
+require __DIR__ . '/inc.php';
 
 //TODO: find all activities which have competencies
 //for these competencies: find out if thy are in local_komettranslator
@@ -25,5 +25,42 @@ require __DIR__.'/inc.php';
 //for each activityid call the function with the related descriptorids
 //done
 //next -> gradings
-block_exacomp_relate_example_to_activity(5,8,array(625));
+
+global $DB;
+
+//relate the modules(activities) to descriptors -> create examples
+//First, get all the activityids that are relevant: all activities that have any competency where the competency exists in local_komettranslator
+$modules = $DB->get_records_sql('
+            SELECT modcomp.cmid as moduleid
+			FROM {competency_modulecomp} modcomp
+            JOIN {local_komettranslator} komet ON komet.internalid = modcomp.competencyid
+			');
+
+//Now we have every relevant module
+//for each module: get the competencies and thereby the descriptors
+foreach ($modules as $module) {
+    // the join on source makes sure, that descriptors with the same sourceid but from different sources, are handled correctly
+//    $descriptors = $DB->get_records_sql('
+//            SELECT komet.id as komettransid, modcomp.competencyid as competencyid, descr.id as descrid, descr.title as descrtitle, descr.sourceid as descrsourceid
+//			FROM {competency_modulecomp} modcomp
+//            JOIN {local_komettranslator}  komet ON komet.internalid = modcomp.competencyid
+//            JOIN {'.BLOCK_EXACOMP_DB_DESCRIPTORS.'} descr ON descr.sourceid = komet.itemid
+//            JOIN {'.BLOCK_EXACOMP_DB_DATASOURCES.'} datasrc ON (datasrc.id = descr.source AND datasrc.source = komet.sourceid)
+//            WHERE modcomp.cmid = ?
+//			', array($module->moduleid));
+    //I keep this for debugging, but I actually only need an array of descriptorids of the local exacomp descriptors
+
+    $descriptors = $DB->get_records_sql('
+            SELECT descr.id as descrid
+			FROM {competency_modulecomp} modcomp
+            JOIN {local_komettranslator}  komet ON komet.internalid = modcomp.competencyid
+            JOIN {' . BLOCK_EXACOMP_DB_DESCRIPTORS . '} descr ON descr.sourceid = komet.itemid
+            JOIN {' . BLOCK_EXACOMP_DB_DATASOURCES . '} datasrc ON (datasrc.id = descr.source AND datasrc.source = komet.sourceid)
+            WHERE modcomp.cmid = ?
+			', array($module->moduleid));
+    $descriptors = array_keys($descriptors); //the keys are the descriptorids, which is what I need
+    block_exacomp_relate_example_to_activity(2, $module->moduleid, $descriptors);
+}
+
+
 
