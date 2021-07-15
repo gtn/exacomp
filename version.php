0be018b57e5185384a5beb634902a0e2d2dac57e
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2021-07-15 10:39:42 +0200

    webservice diwipass_get_sections_with_materials finished

--- version.php
+++ version.php
@@ -20,6 +20,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exacomp';
 $plugin->release = '4.6.5';
-$plugin->version   = 2021071200;
+$plugin->version   = 2021071500;
 $plugin->requires  = 2015051100;
 $plugin->maturity = MATURITY_STABLE;
