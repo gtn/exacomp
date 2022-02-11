704f1c4f8620d156dc527c63ef4a0feeae66a87c
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2022-02-11 11:53:40 +0100

    remove unused observers. version

--- db/upgrade.php
+++ db/upgrade.php
@@ -3986,14 +3986,14 @@ function xmldb_block_exacomp_upgrade($oldversion) {
         upgrade_block_savepoint(true, 2022021000, 'exacomp');
     }
 
-    if ($oldversion < 2022021005) {
+    if ($oldversion < 2022021100) {
         $table = new xmldb_table('block_exacompautotestassign');
         if ($dbman->table_exists($table)) {
             $dbman->drop_table($table);
         }
 
         // Exacomp savepoint reached.
-        upgrade_block_savepoint(true, 2022021005, 'exacomp');
+        upgrade_block_savepoint(true, 2022021100, 'exacomp');
     }
     /*
      * insert new upgrade scripts before this comment section
--- version.php
+++ version.php
@@ -20,6 +20,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exacomp';
 $plugin->release = '4.6.6';
-$plugin->version   = 2022021000;
+$plugin->version   = 2022021100;
 $plugin->requires  = 2015051100;
 $plugin->maturity = MATURITY_STABLE;
