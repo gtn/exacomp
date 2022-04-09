8acd96d271182ff0a6c5dd34513f8acb242e6329
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2022-04-09 12:42:50 +0200

    block_exacomp_get_all_topics() fix for assign_competencies.php

--- version.php
+++ version.php
@@ -20,6 +20,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exacomp';
 $plugin->release = '4.6.6';
-$plugin->version = 2022040700;
+$plugin->version = 2022040900;
 $plugin->requires = 2015051100;
 $plugin->maturity = MATURITY_STABLE;
