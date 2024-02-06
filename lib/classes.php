9b9403085b1078e92328764dbc1741004bb28a53
prowebat <1819129+prowebat@users.noreply.github.com>
2024-02-06 19:36:52 +0800

    fix descriptor sortierung der berichte

--- lib/classes.php
+++ lib/classes.php
@@ -86,8 +86,8 @@ class db_layer {
         }
 
         $sql = "
-			SELECT DISTINCT d.id, d.title, d.source, d.sourceid, d.niveauid, desctopmm.topicid, d.profoundness, d.parentid,
-				n.sorting AS niveau_sorting, n.numb AS niveau_numb, n.title AS niveau_title, dvis.visible as visible, desctopmm.sorting, d.author, d.editor, d.creatorid as descriptor_creatorid
+			SELECT DISTINCT d.id, d.title, d.source, d.sourceid, d.niveauid, desctopmm.topicid, d.profoundness, d.parentid, d.sorting,
+				n.sorting AS niveau_sorting, n.numb AS niveau_numb, n.title AS niveau_title, dvis.visible as visible, d.author, d.editor, d.creatorid as descriptor_creatorid
 			FROM {" . BLOCK_EXACOMP_DB_DESCTOPICS . "} desctopmm
 			JOIN {" . BLOCK_EXACOMP_DB_DESCRIPTORS . "} d ON desctopmm.descrid=d.id AND d.parentid=0
 			-- left join, because courseid=0 has no descvisibility!
