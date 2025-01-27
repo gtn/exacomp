039daf591d8976b3967d8882e232a0ae240189ed
Richard Wolfmayr <richard.wolfmayr@gmail.com>
2025-01-27 14:29:54 +0100

    combine import_additional with normal import task

--- classes/task/import_additional.php
+++ classes/task/import_additional.php
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/*
 namespace block_exacomp\task;
 
 use block_exacomp\data;
@@ -58,3 +59,4 @@ class import_additional extends scheduled_task {
         return true;
     }
 }
+*/
