f7c8076fa82f35dd0de8f320ba6feba1344b217e
rwolf <rwolfmayr@gtn-solutions.com>
2025-08-13 15:27:20 +0200

    bump version

--- version.php
+++ version.php
@@ -20,6 +20,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exacomp';
 $plugin->release = '4.6.7';
-$plugin->version = 2025080701;
+$plugin->version = 2025081300;
 $plugin->requires = 2021051700; // moodle 3.11
 $plugin->maturity = MATURITY_STABLE;
