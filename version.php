4a4407612352bedbb9fc3cb6f40e473385946fb2
rwolf <rwolfmayr@gtn-solutions.com>
2026-07-08 12:03:15 +0200

    bump version for experimental

--- version.php
+++ version.php
@@ -20,6 +20,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_exacomp';
 $plugin->release = '5.1';
-$plugin->version = 2026070800;
+$plugin->version = 2026070801;
 $plugin->requires = 2021051700; // moodle 3.11
 $plugin->maturity = MATURITY_STABLE;
